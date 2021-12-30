c3c3f3278ff9931cd339777cd5b7ee9716cb6124
NikBarykin <barykin.nikita63@gmail.com>
2021-12-30 16:35:04 +0700

    Protocol:: declarations

--- protocol.h
+++ protocol.h
@@ -5,8 +5,42 @@
 
 
 namespace Protocol {
-    std::string SerializeQuery(std::string query_name, std::vector<std::string> arguments);
-    std::pair<std::string, std::vector<std::string>> DeserializeQuery(std::string);
-    std::string SerializeAnswer(std::string error_message, std::string body);
-    std::pair<std::string, std::string> DeserializeAnswer(std::string);
+    struct Query {
+        std::string name_;
+        std::vector<std::string> arguments_;
+
+        Query(std::string name);
+
+        template<class StringIt>
+        Query(std::string name, StringIt arg_begin, StringIt arg_end);
+
+        template<class StringContainer>
+        Query(std::string name, StringContainer arguments);
+
+        std::string Serialize() const;
+        static Query Deserialize(std::string);
+    };
+
+    struct Answer {
+        std::string error_message_;
+        std::string body_;
+
+        Answer(std::string error_message_, std::string body_);
+
+        std::string Serialize() const;
+        static Answer Deserialize(std::string);
+    };
+}
+
+
+// Implementations
+
+namespace Protocol {
+    template<class StringIt>
+    Query::Query(std::string name, StringIt arg_begin, StringIt arg_end)
+    : name_(name), arguments_(arg_begin, arg_end) {}
+
+    template<class StringContainer>
+    Query::Query(std::string name, StringContainer arguments)
+    : Query(name, std::begin(arguments), std::end(arguments)) {}
 }
