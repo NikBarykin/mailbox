4c4d852751c243dce44091b2725091ef983a6082
NikBarykin <barykin.nikita63@gmail.com>
2021-12-30 16:21:37 +0700

    Answer::

--- answer.h
+++ answer.h
@@ -10,35 +10,49 @@
 namespace Answer {
     struct Authorize {
         bool authorization_succeed;
-
+        std::string SerializeForTransfer() const;
         static Authorize DeserializeTransfer(std::string);
     };
 
     struct GetMail {
-        std::vector<Letter> letters;
+        std::vector<Letter> mail;
+        std::string SerializeForTransfer() const;
+        static GetMail DeserializeTransfer(std::string);
     };
 
     struct SendLetter {
-
+        std::string SerializeForTransfer() const;
+        static SendLetter DeserializeTransfer(std::string);
     };
 
     struct Terminate {
-
+        std::string SerializeForTransfer() const;
+        static Terminate DeserializeTransfer(std::string);
     };
 
     struct Error {
-        std::string error_message;
+        std::string message;
+        std::string SerializeForTransfer() const;
+        static Error DeserializeTransfer(std::string);
     };
 
     using Any = std::variant<Authorize, GetMail, SendLetter, Terminate, Error>;
 
     std::string SerializeForTransfer(Any);
-    template<class ExpectedT>
-    Any DeserializeTransfer(std::string transfer) {
-        auto [error_message, body] = Protocol::DeserializeAnswer(transfer);
+    template<class ExpectedAnswer>
+    Any DeserializeTransfer(std::string);
+}
+
+
+// Implementations
+
+namespace Answer {
+    template<class ExpectedAnswer>
+    Any DeserializeTransfer(std::string serialized_ans) {
+        auto [error_message, body] = Protocol::DeserializeAnswer(serialized_ans);
         if (!error_message.empty()) {
             return Error{error_message};
         }
-        return ExpectedT::DeserializeTransfer(transfer);
+        return ExpectedAnswer::DeserializeTransfer(serialized_ans);
     }
 }
